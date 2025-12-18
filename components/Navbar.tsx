d89e5b3b04ef28c69ed3b2346924219bb6bc73e9
diyorbek2005-cyber <153830411+Abuwka05@users.noreply.github.com>
2025-12-18 23:04:09 +0500

    first
    first

---
+++ components/Navbar.tsx
@@ -0,0 +1,15 @@
+import Link from 'next/link';
+
+export default function Navbar() {
+  return (
+    <nav className="border-b mb-6">
+      <ul className="flex gap-6 p-4 font-medium">
+        <li><Link href="/">Home</Link></li>
+        <li><Link href="/lessons">Lessons</Link></li>
+        <li><Link href="/flashcards">Flashcards</Link></li>
+        <li><Link href="/progress">Progress</Link></li>
+        <li><Link href="/community">Community</Link></li>
+      </ul>
+    </nav>
+  );
+}
